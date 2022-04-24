d0ecbe8cccd10dda7443e38e98e1c9dccb7b7347
Pavel Rybakin <pvrybakin@gmail.com>
2022-04-25 01:11:16 +0300

    add auth with next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
+import { SessionProvider as AuthProvider } from 'next-auth/react'
+import { Toaster } from 'react-hot-toast'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	return (
+		<>
+			<AuthProvider session={session}>
+				<Component {...pageProps} />
+			</AuthProvider>
+			<Toaster />
+		</>
+	)
 }
 
-export default MyApp;
+export default MyApp
